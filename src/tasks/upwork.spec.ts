c50dd448d50c00530d3ed9ed05b6418dceff8892
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2021-06-14 15:05:58 +0700

    Correct test

--- src/tasks/upwork.spec.ts
+++ src/tasks/upwork.spec.ts
@@ -1,42 +1,52 @@
-it("maps from one shape to another", () => {
+import objectMapper from 'object-mapper'
+
+it('maps from one shape to another', () => {
   const original = {
     company: {
       devs: [
         {
-          firstname: "Webber",
-          lastname: "Wang",
+          firstname: 'Webber',
+          lastname: 'Wang',
         },
         {
-          firstname: "Vien",
-          lastname: "Nguyen",
+          firstname: 'Vien',
+          lastname: 'Nguyen',
         },
       ],
     },
     info: [
       {
-        key: "user.address.number",
-        value: "1200",
+        key: 'user.address.number',
+        value: '1200',
       },
       {
-        key: "user.address.street",
-        value: "Park ave.",
+        key: 'user.address.street',
+        value: 'Park ave.',
       },
     ],
-  };
+  }
+  const map = {
+    'company.devs': 'devs',
+    'user': original.info.map(item => ({
+      key: item.key,
+      transform: () => item.value
+    }))
+  }
 
   const expected = {
     devs: [
       {
-        firstname: "Webber",
-        lastname: "Wang",
+        firstname: 'Webber',
+        lastname: 'Wang',
       },
       {
-        firstname: "Vien",
-        lastname: "Nguyen",
+        firstname: 'Vien',
+        lastname: 'Nguyen',
       },
     ],
-    user: { address: { number: "1200", street: "Park ave." } },
-  };
+    user: { address: { number: '1200', street: 'Park ave.' } },
+  }
+  const result = objectMapper(original, map)
 
-  expect(original).toStrictEqual(expected);
-});
+  expect(result).toStrictEqual(expected)
+})
